692208f6fe43c71a72c5ed650ae46cafcc9837e4
JeromeL25 <jerylou25@gmail.com>
2025-05-07 23:53:10 +0200

    Add files via upload

---
+++ db_project_2025/docker-lamp-master/www/functions/page5_functions.php
@@ -0,0 +1,13 @@
+<?php
+function getArretsWith($db,$gare,$min_stop){
+    $prep= $db->prepare(
+        "SELECT ARRET.NOM as NOM_ARRET, SERVICE.NOM as NOM_SERVICE, COUNT(TRAJET_ID) as NB_STOP
+        FROM ARRET LEFT JOIN ARRET_DESSERVI ON ARRET_DESSERVI.ARRET_ID = ARRET.ID LEFT JOIN TRAJET ON ARRET_DESSERVI.ITINERAIRE_ID = TRAJET.ITINERAIRE_ID LEFT JOIN SERVICE ON SERVICE.ID=TRAJET.SERVICE_ID
+        WHERE ARRET.NOM LIKE ?
+        GROUP BY NOM_ARRET,SERVICE_ID
+        HAVING NB_STOP>=?
+        ORDER BY NB_STOP DESC;");
+    $prep->execute(['%' . $gare . '%',$min_stop]);
+    return $prep->fetchAll(PDO::FETCH_ASSOC);
+} 
+?>
