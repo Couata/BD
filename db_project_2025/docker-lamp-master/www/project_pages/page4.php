ef70a66c4d60fb28a3042f0a294eff7deb4100d3
JeromeL25 <jerylou25@gmail.com>
2025-05-07 20:07:11 +0200

    Update page4.php
    Border, Write AVG

--- db_project_2025/docker-lamp-master/www/project_pages/page4.php
+++ db_project_2025/docker-lamp-master/www/project_pages/page4.php
@@ -13,20 +13,26 @@
         <a href="../index.html">
             <div class="subbox">Revenir à l'acceuil</div>
     </a>
-        <table>
+        <table border="1">
     <tr><td> Nom Itinéraire </td> <td> Nom Trajet </td> <td> AVG_STOP_TIME </td> </tr>
     <?php
     $tableau = getAllTrajetAvgStopTime($pdo);
     $i=1;
     $name="";
     foreach ($tableau as $l): 
-        if($name!=$l['NOM'])
-            $i=1;
-        else if($l['TRAJET_ID']==NULL)
-            $i=""
+        {if($name!=$l['NOM'])
+            $i=1;    
+        $name=$l['NOM'];
+        if($l['TRAJET_ID']==NULL)
+            $i="MOYENNE";
+        if($l['TRAJET_ID']==NULL&&$l['NOM']==NULL)
+            {
+                $i="";
+                $name="TEMPS MOYEN TOUS TRAJETS";
+            }}
     ?>
         <tr>
-            <td><?= $name=$l['NOM'] ?></td>
+            <td><?= $name?></td>
             <td><?= $i ?></td>
             <td><?= $l['AVG_TIME_STOP'] ?></td>
         </tr>
@@ -35,5 +41,7 @@
     endforeach; 
     ?>
 </table>
+
+        <!-- Insertion de code PHP -->
     </body>
 </html>
